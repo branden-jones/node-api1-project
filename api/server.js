f88cdf35a697046863f00a034acfc8a04628c69d
Branden Jones <branden.wayne.jones@gmail.com>
2022-11-10 11:47:14 -0500

    created get and get by id

--- api/server.js
+++ api/server.js
@@ -1,3 +1,49 @@
 // BUILD YOUR SERVER HERE
+const express = require('express')
+const User = require('./users/model')
+const server = express();
 
-module.exports = {}; // EXPORT YOUR SERVER instead of {}
+server.get('/api/users', (req,res) => {
+    User.find()
+        .then(users => {
+            res.json(users)
+        })
+        .catch(err => {
+            res.status(500).json({
+                message: `Error getting Users`,
+                err: err.message,
+                stack: err.stack
+            })
+        })
+})
+
+server.get('/api/users/:id', (req,res) => {
+    const { id } = req.params;
+    User.findById(id)
+        .then(user => {
+            if (!user){
+                res.status(404).json({
+                    message: "the user with this id does not exist"
+                })
+            }
+            else {
+                res.json(user)
+            }
+        })
+        .catch(err => {
+            res.status(500).json({
+                message: `Error getting Users`,
+                err: err.message,
+                stack: err.stack
+            })
+        })
+})
+
+server.use('*', (req,res) => {
+    res.status(404).json({
+        message: 'not found'
+    })
+})
+
+module.exports = server; // EXPORT YOUR SERVER instead of {}
+
