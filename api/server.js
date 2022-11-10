f51b919da5378ecb030a45e94ae02158616f0ec4
Branden Jones <branden.wayne.jones@gmail.com>
2022-11-10 12:44:39 -0500

    put is solved

--- api/server.js
+++ api/server.js
@@ -4,6 +4,35 @@ const User = require('./users/model')
 const server = express();
 server.use(express.json());
 
+server.put('/api/users/:id', async (req,res) => {
+    try{
+        const { id } = req.params;
+        const possibleUser = await User.findById(id)
+        if(!possibleUser){
+            res.status(404).json({ 
+                message: "The user with the specified ID does not exist"
+            })
+        }
+        else {
+            if(!req.body.name || !req.body.bio) {
+                res.status(400).json({
+                    message: "Please provide name and bio for the user"
+                })
+            }
+            else {
+                const updatedUser = await User.update(id, req.body)
+                res.status(200).json(updatedUser)
+            }
+        }
+    }
+    catch(err) {
+        res.status(500).json({
+            message: "The user information could not be modified",
+            err: err.message
+        })
+    }
+})
+
 server.delete(`/api/users/:id`, async (req,res) => {
     try {
         const { id } = req.params;
